ca8c5cc3f0acb95ed0d0696ed203769f885111b5
antonio <tomgabinio@gmail.com>
2022-09-22 16:30:04 -0300

    Anotações

--- Datetime/main.py
+++ Datetime/main.py
@@ -1,5 +1,12 @@
 from datetime import datetime, timedelta
 
+# .strptime(str, fmt) -> recebe em string e transforma em um horario valido
+# .strftime(fmt) -> vc cria um datetime seguindo as ordens de:
+#       Ano, Mes, Dia, Hora, Minutok, logo apos se deve fazer isso:
+#       ('%d/%m/%Y %H:%M:%S')) <- Formatação por dia, mes, ano, hora, minuto, segundo
+#
+
+
 # data = datetime(2022, 9, 22, 16, 18)
 # print(data.strftime('%d/%m/%Y %H:%M:%S'))
 
