646629ff8935bcf2ca4e9ea72da5785802d8a352
antonio <tomgabinio@gmail.com>
2022-09-22 16:33:17 -0300

    Anotação do timestamp()

--- Datetime/main.py
+++ Datetime/main.py
@@ -1,9 +1,14 @@
 from datetime import datetime, timedelta
 
 # .strptime(str, fmt) -> recebe em string e transforma em um horario valido
+
 # .strftime(fmt) -> vc cria um datetime seguindo as ordens de:
 #       Ano, Mes, Dia, Hora, Minutok, logo apos se deve fazer isso:
 #       ('%d/%m/%Y %H:%M:%S')) <- Formatação por dia, mes, ano, hora, minuto, segundo
+#
+# .timestamp() -> A timestamp is encoded information generally used in UNIX, which indicates the date and time at which a particular event has occurred.
+# This information could be accurate to the microseconds.
+# It is a POSIX timestamp corresponding to the datetime instance.
 #
 
 
