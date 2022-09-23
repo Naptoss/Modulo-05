a4553e4ea8a7dd1bd71b9fb74af55485ae8bf068
antonio <tomgabinio@gmail.com>
2022-09-22 21:32:50 -0300

    adicionado levantamento de erro e o try

--- OS - Modulo/main.py
+++ OS - Modulo/main.py
@@ -1,17 +1,28 @@
 import os
 
 caminho_procura = 'D:\Clips'
-termo_procura = '32'
+termo_procura = 'clip'
 
 for raiz, diretorios, arquivos in os.walk(caminho_procura):
     # vai na raiz e passa verificando o diretorio atras do arquivo
     for arquivo in arquivos:
-        caminho_completo = os.path.join(raiz, arquivo)
+        if termo_procura in arquivo:
+            try:
+                caminho_completo = os.path.join(raiz, arquivo)
+                nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
+                tamanho = os.path.getsize(caminho_completo)
 
-        nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
+                print()
+                print("Encontrei o arquivo:", arquivo)
+                print("Caminho:", caminho_completo)
+                print("Nome:", nome_arquivo)
+                print("Extensão:", ext_arquivo)
+                print("Tamanho:", tamanho)
+            except PermissionError as e:
+                print("Acesso não autorizado.")
 
-        print(nome_arquivo, ext_arquivo, sep='--')
+            except FileNotFoundError as e:
+                print("Arquivo não encontrado. ")
 
-        tamanho = os.path.getsize(caminho_completo)
-        # pega o tamanho do arquivo
-        print(tamanho)
+            except Exception as e:
+                print("Erro desconhecido", e)
