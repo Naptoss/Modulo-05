1800e6143102190a9bedc4e7fedec1bdeb5e1133
antonio <tomgabinio@gmail.com>
2022-10-01 15:40:32 -0300

    Adicionado uma função de gerar senhas aleatoras

--- Random/rd.py
+++ Random/rd.py
@@ -1,5 +1,5 @@
 import random
-
+import string
 
 # Gera um numero inteiro entre A e B
 # inteiro = random.randint(1, 100)
@@ -13,11 +13,18 @@ inteiro = random.randrange(900, 1000, 10)
 # Gera um numero de ponto flutuante entre  0 e 1
 flutuante = random.random()
 
-# Sorteia um nome aleatorio de forma nao repetida
+# Sorteia 2 nomes aleatorio de forma nao repetida
 lista = ['Antonio', 'Allyson', 'Sammy', 'Hugo', 'Luca', 'Felps']
-for i in range(1000):
+for i in range(10):
     sorteio = random.sample(lista, 2)
     print(sorteio)
 
-print(inteiro)
-print(flutuante)
+
+# Gera uma senha aleatoria
+letras = string.ascii_letters
+digitos = string.digits
+caracteres = "!@#$%&*"
+geral = letras + digitos + caracteres
+senha = "".join(random.choices(geral, k=20))
+
+print(senha)
